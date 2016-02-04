a21d12d1c65a4742f6b39f51747e933fdafd5aa2
Tim Horist Jr <Tim Horist Jr>
2016-02-04 13:11:30 -0800

    Set up basic Calculator constructor skeleton; started work on Button constructor

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+//  Begin on document load handler
+$(document).ready(function(){
+
+    //  Begin button click handler
+    $('.buttons').on('click', 'button', function(){
+        calculator.addButtonPress($(this));
+    });
+    //  Close button click handler
+
+});
+//  Close on document load handler
+
+
 //  Begin updateDisplay function
 
 /**
@@ -16,25 +29,70 @@ function updateDisplay(type, value, item) {
 }
 //  Close updateDisplay function
 
-//  Begin global variable my_calculator
-var my_calculator = new calculator(updateDisplay);
-//  Close global variable my_calculator
+//  Begin global object my_calculator
+var calculator = new Calculator(updateDisplay);
+//  Close global object my_calculator
 
-//  Begin on document load handler
-$(document).ready(function(){
+//  Begin Calculator constructor
+function Calculator () {
+    //  Begin main component instantiation
+    var buttons = new Buttons();
+    var memory = new Memory();
+    var display = new Display();
+    //  End main component instantiation
 
-    //  Begin button click handler
-    $('.buttons').on('click', 'button', function(){
-        var val = $(this).text();
-        if (val == 'C') {
-            my_calculator.clear();
-        } else if (val == 'CE') {
-            my_calculator.allClear();
-        } else {
-            my_calculator.addItem(val);
+    //  Begin pass-through method
+    this.addButtonPress = function(buttonDOMObject) {
+        buttons.getButtonPressObject(buttonDOMObject);
+    };
+    //  Close pass-through method
+
+    //  Begin Buttons object constructor
+    function Buttons () {
+        this.getButtonPressObject = function(buttonDOMObject) {
+            var buttonString = getButtonStringFromDOM(buttonDOMObject);
+            var buttonType = getButtonTypeFromString(buttonString);
+        };
+
+        function getButtonStringFromDOM(buttonDOMObject){
+            return buttonDOMObject.text();
         }
-    });
-    //  Close button click handler
 
-});
-//  Close on document load handler
+        function getButtonTypeFromString(buttonString){
+            switch (buttonString) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    return 'operand';
+                case '+':
+                case '-':
+                case '×':
+                case '÷':
+                case '=':
+                    return 'operator';
+                case 'C':
+                case 'CE':
+                    return 'special';
+                default:
+                    return undefined;
+            }
+        }
+    }
+    //  Close Buttons object constructor
+
+    //  Begin Memory object constructor
+    function Memory () {}
+    //  Close Memory object constructor
+
+    //  Begin Display object constructor
+    function Display () {}
+    //  Close Display object constructor
+}
+//  Close Calculator constructor
