1524bcbaf09fb91b9822375e99983bf5b78ba6c8
Tim Horist Jr <Tim Horist Jr>
2016-02-03 18:41:03 -0800

    calculator v0.5 - Tim Horist

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 
-//  Begin thingy function
-function thingy(type, value, item) {
+//  Begin updateDisplay function
+
+/**
+ * Update the display with the given value.
+ * @param {string} type "itemAdded", "calculated", or "error"
+ * @param {number|string|undefined} value
+ * @param item
+ */
+function updateDisplay(type, value, item) {
     if (value === undefined) {
         $('#display').text("");
     } else {
         $('#display').text(value);
     }
-    /*$('#display').text(value);*/
 }
-//  Close thingy function
+//  Close updateDisplay function
 
 //  Begin global variable my_calculator
-var my_calculator = new calculator(thingy);
+var my_calculator = new calculator(updateDisplay);
 //  Close global variable my_calculator
 
 //  Begin on document load handler
 $(document).ready(function(){
+
+    //  Begin button click handler
     $('.buttons').on('click', 'button', function(){
         var val = $(this).text();
-        /*my_calculator.addItem(val);*/
         if (val == 'C') {
             my_calculator.clear();
         } else if (val == 'CE') {
@@ -27,5 +34,7 @@ $(document).ready(function(){
             my_calculator.addItem(val);
         }
     });
+    //  Close button click handler
+
 });
 //  Close on document load handler
