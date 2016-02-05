1bda1bdfad1b70451751ba86709261d76e5de6be
Tim Horist Jr <Tim Horist Jr>
2016-02-05 10:49:57 -0800

    Added input history functionality; evaluation still pending

--- script.js
+++ script.js
@@ -41,13 +41,25 @@ function Calculator () {
     var display = new Display();
     //  End main component instantiation
 
-    //  Begin pass-through method
+    //  Begin addButtonPress controller method
     this.addButtonPress = function(buttonDOMObject) {
         var buttonPress = buttons.getButtonPressObject(buttonDOMObject);
-        console.log('buttonPress object : ', buttonPress);
-        memory.applyButtonPress(buttonPress);
+        var buttonPressConsoleLog = [
+            buttonPress.getString(),
+            buttonPress.getOperationType()
+        ];
+        console.log('buttonPress object : ', buttonPressConsoleLog);
+        var currentMemory = memory.applyButtonPress(buttonPress);
+        var currentMemoryConsoleLog = currentMemory.map(function (object) {
+            var output = [];
+            output.push(object.getValue());
+            output.push(object.getOperationType());
+            output.push(object.getCreationType());
+            return output;
+        });
+        console.log('currentMemory array : ', currentMemoryConsoleLog);
     };
-    //  Close pass-through method
+    //  Close addButtonPress controller method
 
     //  Begin Buttons object constructor
     function Buttons () {
@@ -56,19 +68,21 @@ function Calculator () {
             var buttonType = getButtonTypeFromString(buttonString);
             switch (buttonType) {
                 case "operand":
-                    if (buttonString.charCodeAt(0) >= 48 && buttonString.charCodeAt(0) <= 57) {
-                        return new DigitPress(buttonString);
+                    if (buttonString.charCodeAt(0) >= 49 && buttonString.charCodeAt(0) <= 57) { //Non-zero digits
+                        return new OperandPress(buttonString, null);
+                    } else if (buttonString == '0') {
+                        return new OperandPress(buttonString, null);
                     }
                     return new OperandPress(buttonString);
                 case "operator":
                     switch (buttonString) {
                         case '=':
-                            return new UnaryOperatorPress(buttonString);
+                            return new OperatorPress(buttonString, 1);
                         case '+':
                         case '-':
                         case '×':
                         case '÷':
-                            return new BinaryOperatorPress(buttonString);
+                            return new OperatorPress(buttonString, 2);
                         default:
                             return new OperatorPress(buttonString);
                     }
@@ -110,33 +124,102 @@ function Calculator () {
             }
         }
 
-        function ButtonPress(buttonString){}
-        function OperandPress(buttonString){
-            ButtonPress.call(this, buttonString);
+        function ButtonPress(buttonString, operationType){
+            this.getString = function() {
+                return buttonString;
+            };
+            this.getOperationType = function() {
+                return operationType;
+            };
         }
-        function DigitPress(buttonString){
+        function OperandPress(buttonString, operationStageLimitFunction){
+            ButtonPress.call(this, buttonString, 'operand');
+        }
+        /*function DigitPress(buttonString){
             OperandPress.call(this, buttonString);
+        }*/
+        function OperatorPress(buttonString, numOperands){
+            ButtonPress.call(this, buttonString, 'operator');
         }
-        function OperatorPress(buttonString){
-            ButtonPress.call(this, buttonString);
-        }
-        function UnaryOperatorPress(buttonString){
+        /*function UnaryOperatorPress(buttonString){
             OperatorPress.call(this, buttonString);
         }
         function BinaryOperatorPress(buttonString){
             OperatorPress.call(this, buttonString);
-        }
+        }*/
         function SpecialPress(buttonString){
-            ButtonPress.call(this, buttonString);
+            ButtonPress.call(this, buttonString, 'special');
         }
     }
     //  Close Buttons object constructor
 
     //  Begin Memory object constructor
     function Memory () {
-        var operationList = [];
+        var operationList = [new OperationStage('0', 'operand', 'implicit')];
         this.applyButtonPress = function(buttonPress) {
+            if (operationList.length >= 1) {
+                var appendButtonPressResult = operationList[operationList.length-1].appendButtonPress(buttonPress);
+                console.log("appendButtonPressResult : " + appendButtonPressResult);
+                if (!appendButtonPressResult) {
+                    operationList.push(new OperationStage(buttonPress.getString(), buttonPress.getOperationType(), 'explicit'));
+                    if (buttonPress.getOperationType() == 'operator') {
+                        operationList.push(new OperationStage(
+                            operationList[operationList.length-2].getValue(),
+                            operationList[operationList.length-2].getOperationType(),
+                            'implicit'
+                        ))
+                    }
+                } else if (appendButtonPressResult == 'previous') {
+                    if (operationList.length >= 2) {
+                        appendButtonPressResult = operationList[operationList.length-2].appendButtonPress(buttonPress);
+                        console.log("appendButtonPressResult : " + appendButtonPressResult);
+                    } else {}
+                }
+            } else {
+                operationList.push(new OperationStage(buttonPress.getString(), buttonPress.getOperationType(),'explicit'));
+            }
+            return operationList.slice();
+        };
 
+        function OperationStage (value, operationType, creationType) {
+            this.appendButtonPress = function(buttonPress) {
+                if (buttonPress.getOperationType() == 'operand') {
+                    if (operationType == 'operand') {
+                        if (creationType == 'explicit') {
+                            value += buttonPress.getString();
+                            return 'append';
+                        } else if (creationType == 'implicit') {
+                            value = buttonPress.getString();
+                            if (buttonPress.getString() !== '0') {
+                                creationType = 'explicit';
+                            }
+                            return 'overwrite';
+                        }
+                    } else {
+                        return 'new operation : operand, last stage : not operand';
+                    }
+                } else if (buttonPress.getOperationType() == 'operator') {
+                    if (operationType == 'operand') {
+                        if (creationType == 'implicit') {
+                            return 'previous';
+                        } else if (creationType == 'explicit') {
+                            return false;
+                        }
+                    } else if (operationType == 'operator') {
+                        value = buttonPress.getString();
+                        return 'overwrite';
+                    }
+                }
+                    };
+            this.getValue = function() {
+                return value;
+            };
+            this.getOperationType = function() {
+                return operationType;
+            };
+            this.getCreationType = function() {
+                return creationType;
+            }
         }
     }
     //  Close Memory object constructor
