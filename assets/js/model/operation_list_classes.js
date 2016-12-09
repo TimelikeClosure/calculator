4686e88259c1e6c32cb2843ccdf78dfeaae8eee2
Tim Horist <TimelikeClosure@users.noreply.github.com>
2016-12-09 12:34:50 -0800

    Renamed operation_stages_classes.js to operation_classes.js moved OperationHistory class into its own file

--- assets/js/model/operation_list_classes.js
+++ assets/js/model/operation_list_classes.js
@@ -269,25 +269,3 @@ function OperationList (lastOperation, repeatOperator, repeatOperand) {
     //  Close repeat evaluation operator methods
 }
 //  Close OperationList constructor
-
-//  Begin OperationHistory constructor
-/**
- * Constructs an object for storing previously evaluated OperationLists
- * @constructor
- */
-function OperationHistory () {
-    var history = [];
-
-    this.archiveOperationList = function(operationList) {
-        history.push(operationList);
-    };
-
-    this.getDisplayObject = function() {
-        var displayObject = [];
-        for (var i = history.length - 1; i >= 0; i--) {
-            displayObject.push(history[i].getDisplayObject());
-        }
-        return displayObject;
-    };
-}
-//  Close OperationHistory constructor
